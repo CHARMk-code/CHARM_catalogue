ad110725db8714e24ac36eb42b80ede2f115a7d4
Erik Frennborn <erikfre@student.chalmers.se>
2021-01-09 18:50:22 +0100

    Added status codes

--- docker/flask/src/routes/user/api.py
+++ docker/flask/src/routes/user/api.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, request, jsonify
-from ...models import *
-import csv
+from ...models import Tag, Tag_company, Company
 import sys
 from flask_cors import CORS
 from math import ceil
 from time import time
+from ... import db
+from flask_api import status
 
 last_update_company = ceil(time())
 last_update_tag = ceil(time())
@@ -38,7 +39,7 @@ def tag_add():
         )
         db.session.add(new_tag)
         db.session.commit()
-    return "success"
+    return "success", status.HTTP_200_OK
 
 @blueprint.route("/tag/add")
 def tag_company_add():
@@ -75,7 +76,7 @@ def tag_company_add():
             tag_company.votes +=1
 
     db.session.commit()
-    return "success"
+    return "success", status.HTTP_200_OK
 
 
 @blueprint.route("/tag/match")
@@ -100,7 +101,7 @@ def tag_match():
     if request.args.get("crowd"):
         crowd = int(request.args.get("crowd"))
         if crowd > 2:
-            return
+            return status.HTTP_400_BAD_REQUEST
     select_tags =select_tags.translate({ord('['): None})
     select_tags= select_tags.translate({ord(']'): None})
     select_tags =select_tags.split(',')
@@ -117,7 +118,7 @@ def tag_match():
         for company in companies:
             if Company.query.filter_by(id = company[0], active = True).first():
                 result.append(company)
-    return jsonify(result)
+    return jsonify(result), status.HTTP_200_OK
 
 @blueprint.route("/tag/get")
 def tags_get():
@@ -143,7 +144,7 @@ def tags_get():
     if request.args.get("crowd"):
         crowd = int(request.args.get("crowd"))
         if crowd > 2:
-            return
+            return status.HTTP_401_UNAUTHORIZED
     crowd = 0
     if company_filter:
         t = db.session.query(
@@ -159,7 +160,7 @@ def tags_get():
         crowd = (1==crowd)
         Tag_query = Tag_query.filter_by(crowd_soured = crowd)
     tags = Tag_query.all()
-    return jsonify([tag.serialize for tag in tags])
+    return jsonify([tag.serialize for tag in tags]), status.HTTP_200_OK
 
 # Company
 @blueprint.route("/company/get")
@@ -173,99 +174,6 @@ def companies_get():
     """
     timestamp = request.args.get("timestamp")
     if timestamp:
-        return jsonify(last_update_tag)
+        return jsonify(last_update_tag), status.HTTP_200_OK
     companies = Company.query.filter_by(active = True).all()
-    return jsonify([company.serialize for company in companies])
-    
-# Misc
-@blueprint.route("/load")
-def load():
-    global last_update_tag, last_update_company
-    last_update_company = ceil(time())
-    last_update_tag = ceil(time())
-    """
-    GET endpoint /api/load
-
-    This such be moved to behind authentication.
-
-    When called fills company, tag, tag_company from docker/flask/src/tags.csv & docker/flask/src/data.csv.
-    """
-    with open("tags.csv","r") as csv_file:
-        Company.query.update({Company.active:False})
-        db.session.commit()
-        reader = list(csv.reader(csv_file, delimiter=';', quotechar='|'))
-        row_length = len(reader[0])
-        next_col = 0
-        parent_tag = None
-        for i in range(len(reader)):
-            row = reader[i]
-            tag = Tag.query.filter_by(name=row[next_col]).first()
-            if not tag:
-                new_tag = Tag(
-                    name = row[next_col],
-                    parent_tag = parent_tag,
-                    crowd_soured = False
-                )
-                db.session.add(new_tag)
-                db.session.commit()
-                parent_tag = Tag.query.filter_by(name=row[next_col]).first().id
-            else:
-                parent_tag = tag.id
-
-            if (i+1 >= len(reader)):
-                break
-            if (reader[i+1][next_col]!=''):
-                if (next_col==0):
-                    parent_tag = None
-                continue
-            elif (next_col+1 < row_length):
-                if (reader[i+1][next_col+1]!=''):
-                    next_col += 1
-                    continue
-        
-            for j in range(row_length):
-                if reader[i+1][j] != '':
-                    if (j==0):
-                        parent_tag = None
-                    next_col = j 
-                    break
-                
-                
-            
-
-    with open("data.csv","r") as csv_file:
-        reader = list(csv.reader(csv_file, delimiter=';', quotechar='|'))
-        tags = []
-
-
-        # Generats tags
-        tag_row = reader[0]
-        row_length = len(tag_row)
-        with db.session.no_autoflush:
-            for i in range (row_length -1):
-                tags.append(Tag.query.filter_by(name = tag_row[i+1]).first().id)
-        
-        for i in range(len(reader) -1):
-            if not Company.query.filter_by(name=reader[i+1][0]).first():
-                new_comp = Company(
-                    name = reader[i+1][0],
-                    active = True
-                )
-                db.session.add(new_comp)
-                db.session.commit()
-            comp = Company.query.filter_by(name=reader[i+1][0]).first()
-            comp.active = True
-            db.session.commit()
-            comp_id = comp.id
-            for j in range(row_length-1):
-                if reader[i+1][j+1] == "TRUE":
-                    if not Tag_company.query.filter_by( tag = tags[j],  company = comp_id).first():
-                        new_link = Tag_company(
-                            tag = tags[j],
-                            company = comp_id,
-                            crowd_soured = False,
-                            score = 1,
-                            votes = 1
-                        )
-                        db.session.add(new_link)
-                        db.session.commit()
+    return jsonify([company.serialize for company in companies]), status.HTTP_200_OK
