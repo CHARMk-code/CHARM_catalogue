92d8a517fbd263274b946422f3e647604b869161
Lucas Glimfjord <lucas@glimfjord.com>
2021-10-03 14:27:37 +0200

    Fixes login logic

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
-import "./plugins/axios";
+import axios from "@/plugins/axios";
 import vuetify from "@/plugins/vuetify";
 import Vuex from "vuex";
 //import searchPlugin from "vuex-search";
@@ -15,6 +15,7 @@ Vue.use(Vuex);
 /* eslint-disable no-new */
 new Vue({
   el: "#app",
+  axios,
   router,
   vuetify,
   store,
