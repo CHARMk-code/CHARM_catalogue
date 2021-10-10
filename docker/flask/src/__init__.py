c13725f6fc7ebacdf7b2374943e9da53a7e19dac
ErikFrennborn <erikfre@student.chalmers.se>
2021-10-10 21:06:28 +0200

    Something broken the backend couldn't find what but did some minor change and now it works

--- docker/flask/src/__init__.py
+++ docker/flask/src/__init__.py
@@ -3,12 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mobility import Mobility
 import toml
-import os
-from collections import Mapping
 from flask_cors import CORS
 from .shared_data import last_update_company, last_update_tag
-from math import ceil
-from time import time
 
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
@@ -40,21 +36,8 @@ def create_app():
     from .routes import register_all_blueprints
     register_all_blueprints(app)
 
-    # blueprint for non-auth parts of app
-    # from .main import main as main_blueprint
-    # app.register_blueprint(main_blueprint)
-
-    # adds all routes from the routes folder to the app
-    # from .routes import register_all_blueprints
-    # register_all_blueprints(app)
-    # CORS(app, origins="*", allow_headers=[
-    # "Content-Type", "Authorization", "Access-Control-Allow-Credentials"])
-    global last_update_company, last_update_tag
-    # last_update_company = ceil(time())
-    # last_update_tag = ceil(time())
 
 
-    
     return app
 
 with create_app().app_context():
