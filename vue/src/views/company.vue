8de81428889c5df38aa464bafff73d8bf3abe77a
Lucas Glimfjord <lucas@glimfjord.com>
2021-10-30 14:27:32 +0200

    Reroutes login and upload as well as creates the base for company route

---
+++ vue/src/views/company.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-main>  
+    <v-sheet>
+      
+      {{ $route.params.name }}    
+
+    </v-sheet>
+  </v-main>
+</template>
+
+<script>
+
+export default {
+  components: {
+    
+    //Art,
+    //Logo, 
+    //Business_areas, //Tags?
+    //Trivia, //Did you know...
+    //Founded, //year
+    //Contacts, //name, email, position?
+    //Enployees, //Sweden worldwide
+    //Offering, //Master thesis, summer job, Trainee, Oppotunities abroad, Internship, Recruiting events
+    //Looking_for, //Bachelor, Master, Phd
+    //Programs, // Tags for all programs
+  }
+  
+}
+</script>
+
---
+++ vue/src/views/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-main>
+    <Login />
+  </v-main>
+</template>
+
+<script>
+import Login from "@/components/Login";
+export default {
+  name: "Login view",
+  components: {
+    Login,
+  },
+};
+</script>
