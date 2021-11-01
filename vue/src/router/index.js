add5248d5889b2026fc9d45c06369c9ed75dea6f
ErikFrennborn <erikfre@student.chalmers.se>
2021-11-01 12:31:49 +0100

    Added prepages user view

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -7,6 +7,7 @@ import Administration from "@/views/Administration";
 import Companies from "@/views/admin/Companies";
 import Login from "@/views/login";
 import Upload from "@/components/Upload";
+import Prepage from "@/components/Prepage";
 Vue.use(Router);
 
 export default new Router({
@@ -55,6 +56,11 @@ export default new Router({
       path: "/company/:name",
       name: "Company",
       component: Company_view,
+    },
+    {
+      path: "/prepages/:page",
+      name: "Prepage",
+      component: Prepage,
     },
     {
       path: "/login",
