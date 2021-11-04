3e180a0a0bf99961d2d532c264767ff3e96c3873
ErikFrennborn <erikfre@student.chalmers.se>
2021-11-04 22:32:35 +0100

    Found one more

--- vue/src/components/admin/company_table.vue
+++ vue/src/components/admin/company_table.vue
@@ -96,7 +96,14 @@ export default {
     };
   },
   computed: {
-    ...mapGetters({ companies: "companies/companies", tags: "tags/tags" }),
+    ...mapGetters({
+      companies: "companies/companies",
+      tags: "tags/tags",
+      divisions: "tags/divisions",
+      looking_for: "tags/looking_fors",
+      business_areas: "tags/business_areas",
+      offerings: "tags/offers",
+    }),
     modified_companies() {
       const companies = Array.from(this.companies);
       const modified = companies.map((c) => ({
@@ -149,36 +156,40 @@ export default {
         {
           type: "select",
           model: "divisions",
-          items: this.tags
-            .filter((t) => t.division)
-            .map((t) => ({ text: t.name, value: t.id })),
+          items: this.divisions.map((t) => ({
+            text: t.name,
+            value: t.id,
+          })),
           label: "Divisions",
           hint: "Programs the company are interested in",
         },
         {
           type: "select",
           model: "looking_for",
-          items: this.tags
-            .filter((t) => t.looking_for)
-            .map((t) => ({ text: t.name, value: t.id })),
+          items: this.looking_for.map((t) => ({
+            text: t.name,
+            value: t.id,
+          })),
           label: "Looking For",
           hint: "Which level of education the company is looking for",
         },
         {
           type: "select",
           model: "business_area",
-          items: this.tags
-            .filter((t) => t.business_area)
-            .map((t) => ({ text: t.name, value: t.id })),
+          items: this.business_areas.map((t) => ({
+            text: t.name,
+            value: t.id,
+          })),
           label: "Business areas",
           hint: "The companys business areas",
         },
         {
           type: "select",
           model: "offering",
-          items: this.tags
-            .filter((t) => t.offering)
-            .map((t) => ({ text: t.name, value: t.id })),
+          items: this.offerings.map((t) => ({
+            text: t.name,
+            value: t.id,
+          })),
           label: "offering",
           hint: "Which type of jobs the company is offering",
         },
