17aee1db3dfaee3b89d2ddfa06b9eb4541063ca1
ErikFrennborn <erikfre@student.chalmers.se>
2021-12-01 13:12:28 +0100

    Added completens

--- vue/src/components/admin/company_table.vue
+++ vue/src/components/admin/company_table.vue
@@ -79,9 +79,7 @@ export default {
       headers: [
         { text: "Name", value: "name" },
         { text: "Programs", value: "divisions" },
-        { text: "Business areas", value: "business_area" },
-        { text: "Looking for", value: "looking_for" },
-        { text: "offering", value: "offering" },
+        { text: "Completion", value: "completion" },
         { text: "Active", value: "active", align: "center", width: 110 },
         {
           text: "Actions",
@@ -107,7 +105,7 @@ export default {
     }),
     modified_companies() {
       const companies = Array.from(this.companies);
-      const modified = companies.map((c) => ({
+      let modified = companies.map((c) => ({
         ...c,
         divisions: this.$store.getters["tags/getDivisionsFromIds"](c.tags),
         looking_for: this.$store.getters["tags/getLookingForFromIds"](c.tags),
@@ -117,6 +115,8 @@ export default {
         ),
         tags: this.$store.getters["tags/getTagsFromIds"](c.tags),
       }));
+
+      modified.forEach((c) => (c["completion"] = this.completionCompany(c)));
       return modified;
     },
     row_meta() {
@@ -204,6 +204,22 @@ export default {
     },
     viewCompany(company) {
       this.$router.push("/company/" + company.name);
+    },
+    completionCompany(company) {
+      let missing = 0;
+      if (company.name == "") missing++;
+      if (company.logo == "") missing++;
+      if (company.tags == []) missing++;
+      if (company.talk_to_us_about == "") missing++;
+      if (company.trivia == "") missing++;
+      if (company.website == "") missing++;
+      if (company.founded == -1) missing++;
+      if (company.employees_world == -1) missing++;
+      if (company.employees_sweden == -1) missing++;
+      if (company.description == "") missing++;
+      if (company.contacts == "") missing++;
+
+      return Math.round(((11 - missing) * 100) / 11);
     },
   },
 };
