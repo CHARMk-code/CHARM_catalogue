9244d2b0241ebd3b341438b3de16db5e3893be5d
ErikFrennborn <erikfre@student.chalmers.se>
2021-12-27 10:38:48 +0100

    Minor style fix on landing page

--- vue/src/views/Landing.vue
+++ vue/src/views/Landing.vue
@@ -1,25 +1,47 @@
 <template>
-  <v-row>
-    <v-col>
-      <v-card class="mx-auto" width="600" center to="/prepages/0">
-        <!-- <v-card-title> Look through the catalogue </v-card-title> -->
-        <img contain width="600" center :src="base_URL + prepages[0].image" />
-      </v-card>
-    </v-col>
-    <v-col>
-      <v-card width="500px" height="150px" to="/search">
-        <p class="text-h2" center>Find the company for you!</p>
-      </v-card>
-    </v-col>
-  </v-row>
+  <sideLayout>
+    <container clas="pa-md-4">
+      <v-row>
+        <v-col
+          ><v-container fill-height>
+            <v-card class="mx-auto" width="600" center to="/prepages/0">
+              <!-- <v-card-title> Look through the catalogue </v-card-title> -->
+              <img
+                contain
+                width="600"
+                center
+                :src="base_URL + prepages[0].image"
+              />
+            </v-card>
+          </v-container>
+        </v-col>
+        <v-col>
+          <v-container fill-height>
+            <v-card
+              justify="center"
+              class="mx-auto"
+              width="500px"
+              height="150px"
+              to="/search"
+            >
+              <p class="text-h2" center>Find the company for you!</p>
+            </v-card>
+          </v-container>
+        </v-col>
+      </v-row>
+    </container>
+  </sideLayout>
 </template>
 
 <script>
 import Vue from "vue";
 import { mapGetters } from "vuex";
+import sideLayout from "@/views/sideLayout";
 export default {
   name: "Landing_view",
-  components: {},
+  components: {
+    sideLayout,
+  },
   computed: {
     base_URL() {
       return Vue.prototype.$axios.defaults.baseURL + "/manage/image/";
