3d88b3f8dca782fe94122bd563630f43b6e91c1c
Lucas Glimfjord <lucas@glimfjord.com>
2022-01-16 23:36:15 +0100

    Reformat from prettier and fixes request for snapshot download

--- vue/src/components/admin/Upload.vue
+++ vue/src/components/admin/Upload.vue
@@ -8,43 +8,44 @@
           <div>
             <v-card v-if="this.feedback">{{ this.feedback }}</v-card>
             <v-tooltip bottom max-width="20%">
-              <template v-slot:activator="{ on, attrs }">
+              <template v-slot:activator="{ on }">
                 <div v-on="on">
                   <input type="file" @change="onFileChange" />
                 </div>
               </template>
               <span>
-                Allows .jpg, .png, .svg, .xlsx, .zip, and .tar.gz. For more information please
-                refer to the github.
+                Allows .jpg, .png, .svg, .xlsx, .zip, and .tar.gz. For more
+                information please refer to the github.
               </span>
             </v-tooltip>
           </div>
-           
-            <v-row class="ma-auto">
-              <v-col>
-                <v-btn
-                  @click="onUploadFile"
-                  class="primary ma-auto"
-                  :disabled="!this.selectedFile"
-                >
-                  Upload file
-                </v-btn>
-              </v-col>
-              <v-col> 
-                <v-tooltip bottom max-width="20%">
-                  <template v-slot:activator="{ on, attrs }">
-                    <div v-on="on">
-                      <v-btn
-                        href="https://drive.google.com/drive/folders/1ARqpngACz8koJlrudFBCM7jHow94vemY"
-                        class="primary"
-                      >
+
+          <v-row class="ma-auto">
+            <v-col>
+              <v-btn
+                @click="onUploadFile"
+                class="primary ma-auto"
+                :disabled="!this.selectedFile"
+              >
+                Upload file
+              </v-btn>
+            </v-col>
+            <v-col>
+              <v-tooltip bottom max-width="20%">
+                <template v-slot:activator="{ on }">
+                  <div v-on="on">
+                    <v-btn
+                      href="https://drive.google.com/drive/folders/1ARqpngACz8koJlrudFBCM7jHow94vemY"
+                      class="primary"
+                    >
                       See example
                     </v-btn>
                   </div>
                 </template>
                 <span>
-                  We have provide a example of the formating for a complete upload.
-                  If you want more information please refer to the github page.
+                  We have provide a example of the formating for a complete
+                  upload. If you want more information please refer to the
+                  github page.
                 </span>
               </v-tooltip>
             </v-col>
@@ -52,21 +53,19 @@
         </v-card>
       </v-col>
       <v-col>
-        <v-card  width="500px" align="center">
+        <v-card width="500px" align="center">
           <v-card-title> Download snapshot </v-card-title>
           <v-tooltip bottom max-width="20%">
-            <template v-slot:activator="{ on, attrs }">
+            <template v-slot:activator="{ on }">
               <div v-on="on">
-              <v-btn @click="download" class="primary mb-5">
-                download
-              </v-btn>
+                <v-btn @click="download" class="primary mb-5"> download </v-btn>
               </div>
             </template>
             <span>
-              Downloads all the data need to restore the current state of the catalogue, 
-              including images and a xlsx similarly to the example.
-              Good for when you want a checkpoint before making changes or when you want to
-              save the current catalogue for archiving. 
+              Downloads all the data need to restore the current state of the
+              catalogue, including images and a xlsx similarly to the example.
+              Good for when you want a checkpoint before making changes or when
+              you want to save the current catalogue for archiving.
             </span>
           </v-tooltip>
         </v-card>
@@ -76,7 +75,6 @@
 </template>
 
 <script>
-import Vue from "vue";
 export default {
   name: "Upload",
   data() {
@@ -90,21 +88,23 @@ export default {
       const selectedFile = e.target.files[0]; // accessing file
       this.selectedFile = selectedFile;
     },
-    download(){
-      axios({
-        url: Vue.prototype.$axios.defaults.baseURL+'/manage/download',
-        method: 'GET',
-        responseType: 'blob',
-    }).then((response) => {
-      var fileURL = window.URL.createObjectURL(new Blob([response.data]));
-      var fURL = document.createElement('a');
+    download() {
+      this.$axios({
+        url: "/manage/download",
+        method: "GET",
+        responseType: "blob",
+      })
+        .then((response) => {
+          var fileURL = window.URL.createObjectURL(new Blob([response.data]));
+          var fURL = document.createElement("a");
 
-      fURL.href = fileURL;
-      fURL.setAttribute('download',"CHARM_catalogue_datadump.zip");
-      document.body.appendChild(fURL);
+          fURL.href = fileURL;
+          fURL.setAttribute("download", "CHARM_catalogue_datadump.zip");
+          document.body.appendChild(fURL);
 
-      fURL.click();
-      }).catch((err) => {
+          fURL.click();
+        })
+        .catch((err) => {
           console.log(err);
         });
     },
