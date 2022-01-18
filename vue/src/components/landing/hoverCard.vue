ea6f88a3f83ee8eaeaf00becdc77b494ad16b5f0
Lucas Glimfjord <lucas@glimfjord.com>
2022-01-18 10:17:37 +0100

    Removes unneded code and text on borwse catalogue

--- vue/src/components/landing/hoverCard.vue
+++ vue/src/components/landing/hoverCard.vue
@@ -3,14 +3,13 @@
     <v-card center :to="to">
       <v-img contain :max-width="max_width" center :src="background_image">
         <v-container
-          id="hovereffect2"
+          id="hovereffect"
           fill-height
-          :class="{ 'on-hover': hover, 'white--text': true }"
+          :class="{ 'on-hover': hover, 'white--text': true, 'd-block': true }"
           :style="{
             'background-color': 'rgba(0, 0, 0, 0.35)',
           }"
         >
-          <slot />
         </v-container>
       </v-img>
     </v-card>
@@ -28,26 +27,11 @@ export default {
 </script>
 
 <style scoped>
-/*.v-card {
-  transition: opacity 0.4s ease-in-out;
-}
-
-.v-card:not(.on-hover) {
-  opacity: 0.4;
-}*/
 #hovereffect {
   transition: opacity 0.4s ease-in-out;
-  opacity: 0.75;
 }
 
 #hovereffect:not(.on-hover) {
-  opacity: 0;
-}
-#hovereffect2 {
-  transition: opacity 0.4s ease-in-out;
-}
-
-#hovereffect2:not(.on-hover) {
   opacity: 0;
 }
 </style>
