6a990c7141d9d4339fb7b813ebd842836d34c8a9
ErikFrennborn <erikfre@student.chalmers.se>
2022-03-27 15:21:27 +0200

    Removed token generation, due to breaking

--- docker/flask/src/__init__.py
+++ docker/flask/src/__init__.py
@@ -18,27 +18,27 @@ def create_app():
     app = Flask(__name__)
     # # enable CORS
     CORS(app, origins="*")
-    if config['flask']['secret_key'] == "":
-        def randomString(): 
-            random_string = ''
-            for _ in range(255):
-                # Do while loop to avoid reserved char '
-                random_integer = 39
-                while random_integer == 39 or random_integer == 34:
-                    random_integer = random.randint(32, 127)
-                # Keep appending random characters using chr(x)
-                random_string += (chr(random_integer))
-            
-            return random_string
-
-        # Randomize secrets
-        config['flask']['secret_key'] = randomString()
-        config['creds']['secret'] = randomString()
-
-        with open("config.toml", "w") as toml_file:
-            toml.dump(config,toml_file)
-
-
+    #if config['flask']['secret_key'] == "":
+    #    def randomString(): 
+    #        random_string = ''
+    #        for _ in range(255):
+    #            # Do while loop to avoid reserved char '
+    #            random_integer = 39
+    #            while random_integer == 39 or random_integer == 34:
+    #                random_integer = random.randint(32, 127)
+    #            # Keep appending random characters using chr(x)
+    #            random_string += (chr(random_integer))
+    #        
+    #        return random_string
+#
+    #    # Randomize secrets
+    #    config['flask']['secret_key'] = randomString()
+    #    config['creds']['secret'] = randomString()
+#
+    #    with open("config.toml", "w") as toml_file:
+    #        toml.dump(config,toml_file)
+#
+#
     Mobility(app)
 
     app.config['SECRET_KEY'] = config['flask']['secret_key']
