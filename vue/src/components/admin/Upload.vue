e323f7241d20e8dd10accb4ed55c2a6df1272534
Lucas Glimfjord <lucas@glimfjord.com>
2023-01-06 15:21:09 +0100

    cards side by side instead of above and below

--- vue/src/components/admin/Upload.vue
+++ vue/src/components/admin/Upload.vue
@@ -1,60 +1,71 @@
 <template>
-  <q-page padding>
-    <q-card class="q-mx-auto q-mb-md" style="width: 500px">
-      <q-card-section class="text-h5">
-        Upload data or resources
-      </q-card-section>
-      <q-card-section>
-        Upload a .zip containing a full configuration. Look at the example file
-        for instructions on how it should look
-      </q-card-section>
-      <q-card-section v-if="feedback">
-        <span class="text-bold">File Upload result:</span> {{ feedback }}
-      </q-card-section>
-      <q-form>
+  <q-page
+    padding
+    :class="{
+      row: $q.screen.gt.sm,
+      column: $q.screen.lt.md,
+      'q-col-gutter-md': true,
+    }"
+  >
+    <div class="col-6">
+      <q-card>
+        <q-card-section class="text-h5">
+          Upload data or resources
+        </q-card-section>
         <q-card-section>
-          <q-file
-            filled
-            accept="image/*, .xlsx, .zip, .tar.gz"
-            v-model="selectedFile"
-          >
-            <template v-slot:prepend>
-              <q-icon name="attach_file" />
-            </template>
-          </q-file>
+          Upload a .zip containing a full configuration. Look at the example
+          file for instructions on how it should look
+        </q-card-section>
+        <q-card-section v-if="feedback">
+          <span class="text-bold">File Upload result:</span> {{ feedback }}
+        </q-card-section>
+        <q-form>
+          <q-card-section>
+            <q-file
+              filled
+              accept="image/*, .xlsx, .zip, .tar.gz"
+              v-model="selectedFile"
+            >
+              <template v-slot:prepend>
+                <q-icon name="attach_file" />
+              </template>
+            </q-file>
+          </q-card-section>
+          <q-card-actions :align="'right'">
+            <q-btn
+              href="https://drive.google.com/drive/folders/1ARqpngACz8koJlrudFBCM7jHow94vemY"
+              variant="flat"
+            >
+              Example file
+            </q-btn>
+            <q-btn
+              @click="onUploadFile"
+              variant="flat"
+              color="primary"
+              :disabled="!selectedFile"
+            >
+              Upload file
+            </q-btn>
+          </q-card-actions>
+        </q-form>
+      </q-card>
+    </div>
+    <div class="col-6">
+      <q-card>
+        <q-card-section class="text-h5"> Download snapshot </q-card-section>
+        <q-card-section>
+          Download a snapshot of the current configuration. The configuration is
+          put into a .zip file containing .xlsx with the configuration and all
+          uploaded images used. if the .zip file is uploaded the system will be
+          restored to the state from when it was downloaded. same state
         </q-card-section>
         <q-card-actions :align="'right'">
-          <q-btn
-            href="https://drive.google.com/drive/folders/1ARqpngACz8koJlrudFBCM7jHow94vemY"
-            variant="flat"
-          >
-            Example file
-          </q-btn>
-          <q-btn
-            @click="onUploadFile"
-            variant="flat"
-            color="primary"
-            :disabled="!selectedFile"
-          >
-            Upload file
+          <q-btn @click="download" variant="flat" color="primary">
+            Download snapshot
           </q-btn>
         </q-card-actions>
-      </q-form>
-    </q-card>
-    <q-card class="q-mx-auto" style="width: 500px">
-      <q-card-section class="text-h5"> Download snapshot </q-card-section>
-      <q-card-section>
-        Download a snapshot of the current configuration. The configuration is
-        put into a .zip file containing .xlsx with the configuration and all
-        uploaded images used. if the .zip file is uploaded the system will be
-        restored to the state from when it was downloaded. same state
-      </q-card-section>
-      <q-card-actions :align="'right'">
-        <q-btn @click="download" variant="flat" color="primary">
-          Download snapshot
-        </q-btn>
-      </q-card-actions>
-    </q-card>
+      </q-card>
+    </div>
   </q-page>
 </template>
 
