02f13c6c2626ba2bff49e444b6716183af1fd5c1
ErikFrennborn <erikfre@student.chalmers.se>
2023-01-10 18:15:15 +0100

    Image compent

---
+++ vue/src/components/utils/Image.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-img
+  :src="base_URL + encodeURIComponent(imageName)"
+  v-bind="$props"
+  />
+</template>
+
+<script lang="ts" setup>
+import axios from "@/plugins/axios";
+const base_URL = axios.defaults.baseURL + "/manage/image/";
+const props = defineProps<{
+  imageName: string;
+}>();
+</script>
