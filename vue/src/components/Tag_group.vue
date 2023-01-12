3873f7304c7f6c9fe365c99eeffdd77490c95f49
ErikFrennborn <erikfre@student.chalmers.se>
2023-01-12 18:12:13 +0100

    Some more work, for device swap

--- vue/src/components/Tag_group.vue
+++ vue/src/components/Tag_group.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="">
-    <template v-for="(tag, index) in  data.mutable_tags" >
+    <template v-for="(tag, index) in props.tags" >
       <template v-if="tag.icon && tag.icon.length > 0">
-        <q-avatar size="md" v-bind="props" clickable @click=removeTag(tag)>
+        <q-avatar size="md" v-bind="props" clickable @click=tagClick(tag)>
           <img :src="base_URL + tag.icon" cover />
           <q-tooltip> {{ tag.name }} </q-tooltip>
         </q-avatar>
       </template>
 
       <template v-else>
-        <q-chip size="sm" class="ma-1" clickable @click=removeTag(tag)>
+        <q-chip size="sm" class="ma-1" clickable @click=tagClick(tag)>
           {{ tag.name }}
         </q-chip>
       </template>
@@ -20,32 +20,18 @@
 <script lang="ts" setup>
 import axios from "@/plugins/axios";
 import type { Tag } from "@/stores/modules/tags";
-import { useFilterStore } from "@/stores/modules/filter";
-import { reactive } from "vue";
 const base_URL = axios.defaults.baseURL + "/manage/image/";
-const filterStore = useFilterStore();
-const emit = defineEmits()
+const emit = defineEmits<{
+  (e: "tagClick", tag:Tag): void;
+}>()
 
 const props = defineProps<{
   tags: Tag[];
-  mutable: boolean;
 }>();
 
-const data = reactive({mutable_tags: props.tags});
 
-function removeTag(tag: Tag){
-  if (props.mutable !== true) { return; }
+function tagClick(tag: Tag){
 
-  data.mutable_tags = data.mutable_tags.filter((t) => t.id != tag.id);
-
-  const pre_tags = filterStore.filters.tags;
-  filterStore.filters.tags.divisions = pre_tags.divisions.filter((t) => t != tag.id);
-  filterStore.filters.tags.business_areas = pre_tags.business_areas.filter((t) => t != tag.id);
-  filterStore.filters.tags.fair_areas = pre_tags.fair_areas.filter((t) => t != tag.id);
-  filterStore.filters.tags.offerings = pre_tags.offerings.filter((t) => t != tag.id);
-  filterStore.filters.tags.languages = pre_tags.languages.filter((t) => t != tag.id);
-  filterStore.filters.tags.looking_for = pre_tags.looking_for.filter((t) => t != tag.id);
-  filterStore.filterCompanies();
-  emit("removed");
+  emit("tagClick", tag);
 }
 </script>
--- vue/src/components/company/Filter.vue
+++ vue/src/components/company/Filter.vue
@@ -4,11 +4,11 @@
       <div>
         <template v-if="showFilter">
           Current Filter:
-          <q-chip size="sm" class="ma-1" v-if=showQuery() clickable @click=clearQuery()>
+          <q-chip size="sm" class="ma-1" v-if=showQuery clickable @click=clearQuery()>
             {{filterStore.filters.query}}
           </q-chip>
-          <TagGroup :tags="tags" :mutable="true" @removed="update()"></TagGroup>
-          <q-chip size="sm" class="ma-1" v-if=filterStore.filters.favorites clickable @click=clearFavorit()>
+          <TagGroup :tags="tags" :mutable="true" @tagClick="removeTag"></TagGroup>
+          <q-chip size="sm" class="ma-1" v-if=filterStore.filters.favorites clickable @click=clearFavorite()>
             Favorites
           </q-chip>
           <q-chip size="sm" class="ma-1" v-if=filterStore.filters.sweden  clickable @click=clearSweden()>
@@ -25,41 +25,55 @@
 <script lang="ts" setup>
 import type { Tag } from "@/stores/modules/tags";
 import { useFilterStore } from "@/stores/modules/filter";
-
+import { ref, computed } from "vue";
 import { useTagsStore } from "@/stores/modules/tags";
 import TagGroup from "../Tag_group.vue";
 const filterStore = useFilterStore();
 
 const tagsStore = useTagsStore();
-
-var tags:[Tag] = [];
-var showFilter = false;
-update();
-function update() {
-  const grouped_tags = filterStore.filters.tags;
-  tags = tagsStore.getTagsFromIds([grouped_tags.business_areas,
+const grouped_tags = filterStore.filters.tags;
+const tags:[Tag] = ref(
+  tagsStore.getTagsFromIds([grouped_tags.business_areas,
   grouped_tags.divisions,
   grouped_tags.fair_areas,
   grouped_tags.languages,
   grouped_tags.looking_for,
-  grouped_tags.offerings].flat());
-  showFilter =  showQuery()
+  grouped_tags.offerings].flat()
+));
+
+const showQuery = computed(() => filterStore.filters.query != "")
+const showFilter = ref(false);
+update();
+
+function removeTag(tag: Tag) {
+  tags.value = tags.value.filter((t) => t.id != tag.id);
+
+  const pre_tags = filterStore.filters.tags;
+  filterStore.filters.tags.divisions = pre_tags.divisions.filter((t) => t != tag.id);
+  filterStore.filters.tags.business_areas = pre_tags.business_areas.filter((t) => t != tag.id);
+  filterStore.filters.tags.fair_areas = pre_tags.fair_areas.filter((t) => t != tag.id);
+  filterStore.filters.tags.offerings = pre_tags.offerings.filter((t) => t != tag.id);
+  filterStore.filters.tags.languages = pre_tags.languages.filter((t) => t != tag.id);
+  filterStore.filters.tags.looking_for = pre_tags.looking_for.filter((t) => t != tag.id);
+  filterStore.filterCompanies();
+
+}
+
+function update() {
+   showFilter.value =  showQuery
     || tags.length > 0
     || filterStore.filters.favorites
     || filterStore.filters.sweden
     || filterStore.filters.charmtalk;
 }
 
-function showQuery() {
-  return filterStore.filters.query != ""
-}
 
 function clearQuery() {
   filterStore.filters.query = "";
   filterStore.filterCompanies();
 }
 
-function clearFavorit() {
+function clearFavorite() {
   filterStore.filters.favorites = false;
   filterStore.filterCompanies();
 }
@@ -74,6 +88,7 @@ function clearCharmtalk() {
   filterStore.filterCompanies();
 }
 
+/*
 function removeTag(tag: Tag){
   this.mutable_tags = this.mutable_tags.filter((t) => t.id != tag.id);
 
@@ -86,4 +101,5 @@ function removeTag(tag: Tag){
   filterStore.filters.tags.looking_for = pre_tags.looking_for.filter((t) => t != tag.id);
   filterStore.filterCompanies();
 }
+*/
 </script>
