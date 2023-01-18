c4e71bfedcb3436e7d7fedbba15cb72111655f8d
Lucas Glimfjord <lucas@glimfjord.com>
2023-01-18 13:17:47 +0100

    Improves the company layout

--- vue/src/components/Tag_group.vue
+++ vue/src/components/Tag_group.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="">
-    <template v-for="(tag, index) in props.tags" >
+    <template v-for="(tag, index) in props.tags">
       <template v-if="tag.icon && tag.icon.length > 0">
-        <q-avatar size="md" v-bind="props" clickable @click=tagClick(tag)>
+        <q-avatar size="md" v-bind="props" clickable @click="tagClick(tag)">
           <img :src="base_URL + tag.icon" cover />
           <q-tooltip> {{ tag.name }} </q-tooltip>
         </q-avatar>
       </template>
 
       <template v-else>
-        <q-chip :icon-right="removeable ? 'mdi-close-circle': ''" size="sm" class="ma-1" clickable @click=tagClick(tag)>
+        <q-chip
+          :icon-right="removeable ? 'mdi-close-circle' : ''"
+          size="sm"
+          class="ma-1"
+          clickable
+          @click="tagClick(tag)"
+        >
           {{ tag.name }}
         </q-chip>
       </template>
@@ -22,17 +28,18 @@ import axios from "@/plugins/axios";
 import type { Tag } from "@/stores/modules/tags";
 const base_URL = axios.defaults.baseURL + "/manage/image/";
 const emit = defineEmits<{
-  (e: "tagClick", tag:Tag): void;
-}>()
-
-const props = defineProps<{
-  tags: Tag[];
-  removeable: boolean;
+  (e: "tagClick", tag: Tag): void;
 }>();
 
+const props = withDefaults(
+  defineProps<{
+    tags: Tag[];
+    removeable?: boolean;
+  }>(),
+  { removeable: false }
+);
 
-function tagClick(tag: Tag){
-
+function tagClick(tag: Tag) {
   emit("tagClick", tag);
 }
 </script>
