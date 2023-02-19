c8e3b63bd22fed3f1288b1245fc7f873d4517991
Lucas Glimfjord <lucas@glimfjord.com>
2023-02-19 12:26:17 +0100

    Fixes eslint configuration

--- vue/.eslintrc.cjs
+++ vue/.eslintrc.cjs
@@ -2,10 +2,16 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-  root: true,
-  extends: [
-    "plugin:vuetify/base",
+  "root": true,
+  "parser": "vue-eslint-parser",
+  "parserOptions": {
+    "parser": "@typescript-eslint/parser",
+  },
+  "extends": [
+    "eslint:recommended",
     "plugin:vue/vue3-recommended",
-    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-typescript/recommended",
+    "prettier",
   ],
+  "plugins": ["@typescript-eslint"],
 };
