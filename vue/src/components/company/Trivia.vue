b0fab528070d211b49b317aedca6dd3d7e56490f
ErikFrennborn <erikfre@student.chalmers.se>
2023-11-13 00:54:49 +0100

    Tempary removed 'we have' from trivia for cate-it

--- vue/src/components/company/Trivia.vue
+++ vue/src/components/company/Trivia.vue
@@ -8,18 +8,6 @@
       <div class="q-pb-sm">
         {{ talkToUsAbout }}
       </div>
-      <div v-if="location" class="q-pb-sm">
-          <span class="text-bold"> We have</span> offices in {{ location }}
-      </div>
-      <div v-if="sweden != -1" class="q-pb-sm">
-        <span class="text-bold">We have</span> {{ sweden }} employees in Sweden
-      </div>
-      <div v-if="world != -1" class="q-pb-sm">
-        <span class="text-bold">We have</span> {{ world }} employees worldwide
-      </div>
-      <div v-if="founded" class="q-pb-sm">
-        <span class="text-bold"> We were</span> founded in {{ founded }}
-      </div>
     </q-card-section>
   </company_card_wrapper>
 </template>
